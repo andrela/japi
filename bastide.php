ae18df87b1f0df2c72c340d0a0512d05253f27ea
Lasfargues <altuxbio@gmail.com>
2010-05-24 19:34:12 +0200

    corrections

--- bastide.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn_string = "host=localhost port=5432 dbname=bastide user=andre password=loboG69";
-$dbconn = pg_pconnect($conn_string);
-
-if (!$dbconn) {
-    echo "An error occured.\n";
-    exit;
-}
-?>
